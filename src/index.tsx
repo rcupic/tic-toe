0ad6e602429b7361f4144eeab219c1f35a3ec25a
Roko Čupić <rcupic@profico.hr>
2022-01-25 16:29:34 +0100

    add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Game } from './components/Game';
 import './index.scss';
+import { Posts } from './routes/posts';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/game" element={<Game />} />
+      <Route path="/posts" element={<Posts />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
---
+++ src/routes/posts.tsx
@@ -0,0 +1,7 @@
+export const Posts = function () {
+  return (
+    <div style={{ padding: '1rem 0' }}>
+      <h2>Posts</h2>
+    </div>
+  );
+};
